7fd226407b1aee25cdbb8c38b190eb4850d18892
Mikhail Gusarov <dottedmag@dottedmag.net>
2009-09-03 03:55:31 +0700

    Cleanup autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -3,10 +3,7 @@
 rm -rf autom4te.cache
 rm -f aclocal.m4 ltmain.sh
 
-#touch README
-
 echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
-#echo "Running autoheader..." ; autoheader || exit 1
 echo "Running autoconf..." ; autoconf || exit 1
 echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
 echo "Running automake..." ; automake --add-missing --copy --gnu || exit 1
